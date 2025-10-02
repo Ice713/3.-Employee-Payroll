8fd766043b49b32f18c885ce0eee97bab58678a1
Ice713 <icramzilruiz@gmail.com>
2025-10-02 09:51:34 +0800

    Working with Employee Class

---
+++ 3. Employee Payroll/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3.Employee_Payroll
+{
+    internal class Employee
+    {
+        public string Name { get; set; }
+        public double Salary { get; set; }
+    }
+}
