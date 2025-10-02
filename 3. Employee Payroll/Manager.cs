30a54f1e15ff28c4adf70b13a9195fe31f973c65
Ice713 <icramzilruiz@gmail.com>
2025-10-02 09:54:01 +0800

    Manager Class Done

---
+++ 3. Employee Payroll/Manager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.Employee_Payroll
+{
+    internal class Manager : Employee
+    {
+        public string Department { get; set; }
+        public Manager(string name, double salary, string department)
+            : base(name, salary)
+        {
+            Department = department;
+        }
+        public string GetDetails()
+        {
+            return $"{GetDetails()}\nDepartment: {Department}";
+        }
+    }
+    {
+
+    }
+}
